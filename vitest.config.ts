a79d8d28785790767226d8268beecc9e586f8cab
snowingfox <1503401882@qq.com>
2023-07-20 06:57:57 +0000

    fix: fix coverage error

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
     optimizeDeps: {
         entries: [],
+    },
+    test: {
+        coverage: {
+            reporter: ['lcov']
+        },
     }
 })
